e378b193eef22aa546f54b2ccc186a708b0d65b2
Quan Dao <daominhquan9909@gmail.com>
2019-03-18 15:01:50 +0100

    Finish tracking

--- ecn_tracking/tracking_video_f2f.py
+++ ecn_tracking/tracking_video_f2f.py
@@ -6,27 +6,33 @@ import numpy as np
 
 cap = cv.VideoCapture('video1.mp4')
 
-query_img = cv.imread('first_frame.png', 0)
-
 MIN_MATCH_COUNT = 10  # minimum number of found mathc points
 
 # Create SURF object
 surf = cv.xfeatures2d.SURF_create()
 surf.setHessianThreshold(400)
 
-# Creat mask to restrict search for keypoint 
-x, y, w, h = 24, 46, 170, 160  # hard-coded bounding box
-mask = np.zeros(query_img.shape)
-mask[x:x+w, y:y+h] = 1
-
-# Find keypoints & descriptors
-kp_query, des_query = surf.detectAndCompute(query_img, mask.astype(np.uint8))
-
 # FLANN parameters
 FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
 search_params = dict(checks=50)   # or pass empty dictionary
 flann = cv.FlannBasedMatcher(index_params,search_params)
+
+
+def findQueryKeypoints(query_img, box_array):
+	# ROI in query_img
+	box_array = np.int32(np.round(box_array))
+	x = box_array[0, 0]
+	y = box_array[0, 1]
+	w = box_array[3, 0] - box_array[0, 0] + 1
+	h = box_array[1, 1] - box_array[0, 1] + 1
+	# create mask
+	mask = np.zeros(query_img.shape)
+	mask[x: x+w, y: y+h] = 1
+	# Find keypoints & descriptors
+	kp_query, des_query = surf.detectAndCompute(query_img, mask.astype(np.uint8))
+	return kp_query, des_query
+
 
 
 def findMatchedKeyPoints(train_img, des_query):
@@ -44,7 +50,7 @@ def findMatchedKeyPoints(train_img, des_query):
 	return good_matches, kp_train
 
 
-def localizeObject(kp_query, kp_train, good_matches, original_bounding_box=(x, y, w, h)):
+def localizeObject(kp_query, kp_train, good_matches, box_array):
 	# Extract good mathces kp in query & train img
 	src_pts = np.float32([kp_query[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2)
 	dst_pts = np.float32([kp_train[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2)
@@ -52,29 +58,54 @@ def localizeObject(kp_query, kp_train, good_matches, original_bounding_box=(x, y
 	M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
 	matchesMask = mask.ravel().tolist()
 
-	x, y, w, h = original_bounding_box
-	pts = np.float32([[x, y],
-					[x, y + h - 1],
-					[x + w - 1, y + h - 1],
-					[x + w - 1, y]]).reshape(-1,1,2)
+	pts = box_array.reshape(-1, 1, 2)
+
 	dst = cv.perspectiveTransform(pts, M)
 	return dst
 
 
+# Initial value
+query_img = cv.imread('first_frame.png', 0)  
+x, y, w, h = 24, 46, 170, 160
+box_array = np.float32([[x, y],
+					  [x, y+h-1],
+					  [x+w-1, y+h-1],
+					  [x+w-1, y]])
+
 while cap.isOpened():
 	ret, frame = cap.read()
 	if ret:
+		# Get train_img
 		train_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+		# Find keypoints & descriptors in query_img
+		kp_query, des_query = findQueryKeypoints(query_img, box_array)
+		
+		# Find matched keypoints
 		good_matches, kp_train = findMatchedKeyPoints(train_img, des_query)
-		if len(good_matches) > MIN_MATCH_COUNT:
-			dst = localizeObject(kp_query, kp_train, good_matches)
+		
+		if len(good_matches) >= MIN_MATCH_COUNT:
+			dst = localizeObject(kp_query, kp_train, good_matches, box_array)
+			# Update query_img & box_array
+			query_img = train_img
+			box_array = dst.squeeze()
 			# draw bounding box for found object
-			train_img = cv.polylines(train_img, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+			tracked_img = cv.polylines(train_img, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+			# Display
+			cv.imshow('tracked', tracked_img)
 		else:
 			print "Not enough matches are found - %d/%d" % (len(good_matches),MIN_MATCH_COUNT)
 			matchesMask = None
-
-		cv.imshow('tracked', train_img)
+			# # reset query_img & box_array
+			# query_img = cv.imread('first_frame.png', 0)  
+			# x, y, w, h = 24, 46, 170, 160
+			# box_array = np.float32([[x, y],
+			# 					  [x, y+h-1],
+			# 					  [x+w-1, y+h-1],
+			# 					  [x+w-1, y]])
+			# Display
+			cv.imshow('tracked', train_img)
+		
 		cv.waitKey(1)
 	else:
 		break
